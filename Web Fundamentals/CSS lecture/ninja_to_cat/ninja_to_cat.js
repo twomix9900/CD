b9497248c0592c49b443a5b6dd62d165d174665b
Kay K <26286225+twomix9900@users.noreply.github.com>
2018-05-24 12:00:28 -0700

    update ninja_to_cat, add normalization mwb

--- Web Fundamentals/CSS lecture/ninja_to_cat/ninja_to_cat.js
+++ Web Fundamentals/CSS lecture/ninja_to_cat/ninja_to_cat.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   $("img").css("display","block");
   $("img").click(function() {
+    let temp = $(this).attr("src");
     $(this).attr("src",$(this).attr("data-alt-src"));
+    $(this).attr("data-alt-src",temp);
   })
 })
