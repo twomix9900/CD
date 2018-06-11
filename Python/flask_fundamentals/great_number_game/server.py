b9f34ef0ac0280435c849d4f26211f1968829809
Kay K <26286225+twomix9900@users.noreply.github.com>
2018-06-10 18:07:35 -0700

    add the great number game

--- Python/flask_fundamentals/great_number_game/server.py
+++ Python/flask_fundamentals/great_number_game/server.py
@@ -1,10 +1,29 @@
-from flask import Flask, redirect, session, render_template
+from flask import Flask, redirect, session, render_template, request, flash
+import random
 
 app = Flask(__name__)
+app.secret_key = "ThisIsSomeTopSecretStuff"
 
 @app.route("/")
+def root():
+  session["number"] = random.randrange(0, 101)
+  return render_template("index.html")
+
+@app.route("/index")
 def index():
   return render_template("index.html")
 
-if __name__ == "main":
+@app.route("/process", methods=["POST"])
+def process():
+  if "guess" in request.form:
+    session["guess"] = int(request.form["guess"])
+  return redirect("/index")
+
+@app.route("/reset", methods=["POST"])
+def reset():
+  session["number"] = random.randrange(0, 101)
+  session["guess"] = False
+  return redirect("/index")
+
+if __name__ == "__main__":
   app.run(debug=True)
